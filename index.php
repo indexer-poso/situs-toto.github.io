c31572090736b9acab65321921d09a398a7a8caa
POSOTOTO <indexer.poso@gmail.com>
2025-04-05 21:40:31 +0700

    index.php

---
+++ index.php
@@ -0,0 +1,71 @@
+
+<!DOCTYPE html>
+<html xmlns="http://www.w3.org/1999/xhtml" lang="id">
+
+<head>
+  <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+  <meta name="google-site-verification" content="CLXuqrXIziQOSjUudsmlOWcBl0Wa2rUU4lryoNjGzqs" />
+  <meta name="viewport" content="initial-scale=1.0" />
+  <title>POSOTOTO: Situs Toto Resmi Terpercaya Indonesia</title>
+  <meta name="description"
+    content="POSOTOTO merupakan situs toto resmi terpercaya dengan pilihan pasaran terlengkap dan hadiah terbesar." />
+  <meta property="og:locale" content="id_ID" />
+  <meta property="og:type" content="website" />
+  <meta property="og:title" content="POSOTOTO: Situs Toto Resmi Terpercaya Indonesia" />
+  <meta property="og:description"
+    content="POSOTOTO merupakan situs toto resmi terpercaya dengan pilihan pasaran terlengkap dan hadiah terbesar." />
+  <meta property="og:url" content="https://www.posototo.com/" />
+  <meta property="og:site_name" content="POSOTOTO" />
+  <meta property="og:image" content="https://www.posototo.com/poso.png" />
+  <meta name="twitter:card" content="summary" />
+  <meta name="twitter:title" content="POSOTOTO: Situs Toto Resmi Terpercaya Indonesia" />
+  <meta name="twitter:description"
+    content="POSOTOTO merupakan situs toto resmi terpercaya dengan pilihan pasaran terlengkap dan hadiah terbesar." />
+  <link rel="canonical" href="https://www.posototo.com/">
+  <link rel="shortcut icon" href="https://seo-poso.io/img/posototo/favicon.png">
+  <link href="https://fonts.googleapis.com/css?family=Prompt:200,400" rel="stylesheet">
+  <style> body { line-height: 1; background: #6e1313; color: #fff; font-family: prompt, arial } ol, ul { list-style: none } blockquote, q { quotes: none } blockquote : after, blockquote : before, q : after, q : before { content: ''; content: none } #bodywrap { width: 900px; margin: 0 auto } #headwrap { width: 100%; text-align: center; padding-top: 15px } .logo img { max-width: 60% } #news { width: 100%; font-family: prompt, arial; text-transform: uppercase } .slide { margin-bottom: 10px } h1, h2, h3 { margin-bottom: 20px; text-transform: uppercase; letter-spacing: 1.5px; color: #d8d8d7 } h1 { font-size: 10px; font-family: "JetBrains Mono", monospace; } h2 { font-size: 22px } h3 { font-size: 20px } p { line-height: 1.8; margin-bottom: 18px } a : hover, a : link, a : visited { color: #000; text-decoration: none } img { max-width: 100% } .date { font-size: 12px; background: #111; padding: 5px 0 5px 10px; border: 1px solid #333; border-radius: 5px; color: #ccc; margin-bottom: 10px } .copyright { max-width: 600px; margin: 15px auto 30px; text-align: center; font-size: 14px; color: #fff; width: 100% } .tombol { float: right; width: 35px } #loginForm { margin: 10px auto; width: 250px } #loginForm input { height: 42px; width: 100% } #main img { border: 3px solid #f8c10a; border-radius: 10px; } #btnRegisterposo, #submitbttn { width: 100%; height: 42px; line-height: 42px; text-align: center; display: inline-block; vertical-align: middle; border: 0; border-radius: 10px; cursor: pointer; border: 1px solid #000 } #submitbttn { margin-top: 10px } #btnRegisterposo { background: #f8c10a } .tmbolBtn { align-items: center; appearance: none; background-color: #f8c10a; border-radius: 4px; border-width: 0; box-shadow: rgba(45, 35, 66, 0.4) 0 2px 4px, rgba(45, 35, 66, 0.3) 0 7px 13px -3px, #D6D6E7 0 -3px 0 inset; box-sizing: border-box; color: #3d3d3d; cursor: pointer; display: inline-flex; height: 48px; justify-content: center; line-height: 1; list-style: none; overflow: hidden; padding-left: 16px; padding-right: 16px; position: relative; text-align: left; text-decoration: none; transition: box-shadow .15s, transform .15s; user-select: none; -webkit-user-select: none; touch-action: manipulation; white-space: nowrap; will-change: box-shadow, transform; font-size: 18px; } .tmbolBtn:focus { box-shadow: #D6D6E7 0 0 0 1.5px inset, rgba(45, 35, 66, 0.4) 0 2px 4px, rgba(45, 35, 66, 0.3) 0 7px 13px -3px, #D6D6E7 0 -3px 0 inset; } .tmbolBtn:hover { box-shadow: rgba(236, 5, 5, 0.4) 0 4px 8px, rgba(45, 35, 66, 0.3) 0 7px 13px -3px, #D6D6E7 0 -3px 0 inset; transform: scale(0.5,0.5); } .tmbolBtn:active { box-shadow: #D6D6E7 0 3px 7px inset; transform: translateY(4px); } .tmbolBtn-daftar { color: #3d3d3d } .formInput { padding: 0 5px } input, select, textarea { box-sizing: border-box; -moz-box-sizing: border-box; -webkit-box-sizing: border-box } @media screen and (max-width :480px) { #bodywrap { max-width: 96%; margin: 0 auto } #news { font-size: 14px; height: 20px } .daftar, .login { width: 65%; margin: 10px auto; height: 37px; line-height: 35px; font-size: 16px; font-weight: 700 } p { text-align: justify } } @media screen and (max-width :320px) { #bodywrap { max-width: 96%; margin: 0 auto } p { text-align: justify } } </style>
+  <script type='application/ld+json'>
+{"@context":"http:\/\/schema.org","@type":"WebSite","@id":"#website","url":"https://www.posototo.com/","name":"POSOTOTO: Situs Toto Resmi Terpercaya Indonesia","alternateName":"POSOTOTO"}
+</script>
+  <link rel="amphtml" href="https://amp.seo-poso.io/utama.html">
+</head>
+
+<body oncontextmenu="return false;" onkeydown="return false;" onmousedown="return false;">
+  <div id="bodywrap">
+    <div id="headwrap">
+      <div class="logo">
+        <a href="https://www.posototo.com/"><img src="https://seo-poso.io/img/posototo/logo.png" alt="Logo POSOTOTO" width="150"
+            height="70" /></a>
+
+      </div>
+      <div id="news">
+        <marquee> ~ Situs Slot Online Gacor Maxwin Tanpa Batas ~ Waspada Situs Palsu ~ Bermain selalu di Link Asli POSOTOTO~
+        </marquee>
+      </div>
+    </div>
+    <div id="main" style="text-align:center">
+      <img src="https://seo-poso.io/img/posototo/welcome.webp" width="400" height="300" alt="POSOTOTO Slot Online Gacor">
+      <br>
+      <h1>POSOTOTO SITUS TOTO RESMI</h1>
+      <table id="loginForm">
+
+        <tr>
+          <td><a href="https://posototo.site/alt.php" rel="nofollow noreferrer noopener" target="_blank"
+              id="submitbttn" class="tmbolBtn" method="login" style="cursor: pointer;">DAFTAR POSOTOTO</a>
+          </td>
+        </tr>
+        <tr>
+          <td><a href="https://posototo.site/alt.php" rel="nofollow nofollow noreferrer noopener" target="_blank"
+              id="daftarposo" class="tmbolBtn tmbolBtn-daftar" method="register">LOGIN POSOTOTO</a>
+          </td>
+        </tr>
+      </table>
+      <br>
+
+
+    </div>
+  </div>
+</body>
+
+</html>
